a64cf6aa2c47c6f72335f5debcade6867f26450d
Veen Lorivz <veenlorivz194@gmail.com>
2021-08-07 13:56:52 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const HOURS = document.querySelector(".jam");
+const MINUTE = document.querySelector(".menit");
+
+const waktuSekarang = function () {
+  const date = new Date();
+  let jam = 10;
+  const menit = date.getMinutes();
+
+  setTimeout(() => {
+    if (jam >= 12) {
+      jam -= 12;
+      HOURS.textContent = `${jam} `;
+      MINUTE.textContent = `${menit}`;
+    } else {
+      HOURS.textContent = `${jam} `;
+      MINUTE.textContent = `${menit}`;
+    }
+
+    const colon = document.querySelector(".colon");
+    colon.classList.toggle("kedip");
+
+    waktuSekarang();
+  }, 1000);
+};
+
+waktuSekarang();
